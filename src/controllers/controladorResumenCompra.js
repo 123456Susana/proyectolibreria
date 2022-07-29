4b475037dff7135033d5366d9747e50bf9b43322
308 <308@academicos.cesde.edu.co>
2022-07-29 12:06:43 -0500

    se crea boton limpiar

--- src/controllers/controladorResumenCompra.js
+++ src/controllers/controladorResumenCompra.js
@@ -5,16 +5,22 @@ let carrito=JSON.parse(localStorage.getItem('carrito'))
 
 
 if(carrito==null){
-
     pintarResumenCompra("../../assets/img/nohay.webp", "Carrito vacio", true, false,null,false,null)
-
 }
 else { //el carrito esta lleno
     //recorro el carrito de compras
     carrito.forEach(function(producto){
-
-    pintarResumenCompra(producto.foto,producto.nombre,false,true,producto.precio,true,producto.cantidad)
-   
-    
+    pintarResumenCompra(producto.foto,producto.nombre,false,true,producto.precio,true,producto.cantidad)   
 })
 }
+
+//RUTINA PARA LIMPIAR EL RESUMEN DE LA COMPRA
+let limpiar=document.getElementById("limpiar")
+limpiar.addEventListener("click", function(evento){
+    localStorage.removeItem("carrito")
+
+    let contenedor=document.getElementById("contenedor")
+    contenedor.innerHTML="" 
+
+    pintarResumenCompra("../../assets/img/nohay.webp", "Carrito vacio", true, false,null,false,null)
+})
